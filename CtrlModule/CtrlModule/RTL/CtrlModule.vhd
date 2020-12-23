c46b839b803444ec3f0c06d1686e7eef6b103ed0
NeuroRulez <neuro_999@hotmail.com>
2020-12-23 09:53:59 +0100

    Add @

--- CtrlModule/CtrlModule/RTL/CtrlModule.vhd
+++ CtrlModule/CtrlModule/RTL/CtrlModule.vhd
@@ -1,3 +1,7 @@
+--------------------------------------------------------------
+-- Control Module to simulate a Mist-Arm menu by NeuroRulez --
+--------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
